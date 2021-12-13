baa9ec21c07ae011516c5817df847759a61bd3d1
alexandr <dylan_alexandre@outlook.fr>
2021-12-13 03:28:00 +0100

    Implémentation des fonctions print et eval

---
+++ affichage.ml
@@ -0,0 +1,38 @@
+open Include
+
+let rec print_ind (ind:int) : string =
+  match ind with 
+  | 0 -> ""
+  | _ ->  "  " ^ (print_ind (ind-1))
+
+let print_op (o:op) : string =
+  match o with 
+  | Add -> "+ " | Sub -> "- " | Mul -> "* " | Div -> "/ " | Mod -> "% " 
+
+let print_comp (c:comp) : string = 
+  match c with
+  | Eq -> "= "| Ne -> "<> " | Lt -> "< " | Le -> "<= " | Gt -> "> " | Ge -> ">= " 
+
+let rec print_expr (e:expr) : string =
+  match e with 
+  | Num(i) -> string_of_int (i)
+  | Var(v) -> v
+  | Op(a,b,c) -> let n = print_op a
+      in n ^ (print_expr b) ^ " " ^ (print_expr c)
+                                    
+let print_cond (c:cond) : string =
+  match c with 
+  | (x,y,z) -> print_expr x ^ " " ^ print_comp y ^ print_expr z
+               
+let print_block (prog:program) : unit = 
+  let rec aux prog (ind:int) =
+    match prog with 
+    | [] -> ()
+    | (x,y)::r -> match y with 
+      | Set(name,expr) -> (Printf.printf "%s%s := %s\n" (print_ind ind)  name (print_expr expr)); (aux r ind)
+      | Read(name) -> Printf.printf "%sREAD %s\n" (print_ind ind) name; (aux r ind)
+      | Print(expr) -> Printf.printf "%sPRINT %s\n" (print_ind ind) (print_expr expr); (aux r ind)
+      | If(cond,block1,block2) -> if (List.length block2) = 0 then ((Printf.printf "%sIF %s\n" (print_ind ind) (print_cond cond)); (aux block1 (ind+1)); (aux r ind))
+            else ((Printf.printf "%sIF %s\n" (print_ind ind) (print_cond cond)); (aux block1 (ind+1)); (Printf.printf "%sELSE\n" (print_ind ind));(aux block2 (ind+1));(aux r ind))
+      | While(cond,block) ->(Printf.printf "%sWHILE %s\n" (print_ind ind) (print_cond cond)); (aux block (ind+1)); (aux r ind)
+  in aux prog 0
---
+++ evaluation.ml
@@ -0,0 +1,38 @@
+open Include
+
+type env = (name * int) list
+
+let rec eval_expr (e:expr) (env:env) : int =
+  match e with 
+  | Num(x) -> x
+  | Var(x) -> begin try List.assoc x env with Failure e -> failwith "Cette variable n'existe pas." end
+  | Op(a,b,c) -> match a with 
+    | Add -> ((eval_expr b env) + (eval_expr c env))
+    | Sub -> ((eval_expr b env) - (eval_expr c env)) 
+    | Mul -> ((eval_expr b env)  * (eval_expr c env))
+    | Div -> begin try ((eval_expr b env) / (eval_expr c env)) with Failure e -> failwith "Division par 0 impossible." end
+    | Mod -> begin try ((eval_expr b env) mod (eval_expr c env)) with Failure e -> failwith "Mod par 0 impossible." end
+
+let eval_cond (c:cond) (env:env) : bool =
+  match c with 
+  | (expr1,Eq,expr2) -> (eval_expr expr1 env) = (eval_expr expr2 env)
+  | (expr1,Ne,expr2) -> (eval_expr expr1 env) <> (eval_expr expr2 env) 
+  | (expr1,Lt,expr2) -> (eval_expr expr1 env) < (eval_expr expr2 env)
+  | (expr1,Le,expr2) -> (eval_expr expr1 env) <= (eval_expr expr2 env) 
+  | (expr1,Gt,expr2) -> (eval_expr expr1 env) > (eval_expr expr2 env)
+  | (expr1,Ge,expr2) -> (eval_expr expr1 env) >= (eval_expr expr2 env)
+
+
+let eval_block (prog:program) : unit =
+  let a = 
+  let rec aux prog (env:env) : env =
+    match prog with 
+    | [] -> env
+    | (x,y)::r -> match y with 
+      | Set(name,expr) -> if List.mem_assoc name env then (aux r ((name,eval_expr expr env)::env)) else aux r ((name,eval_expr expr env)::env)
+      | Read(name) -> print_string name; print_string "?\n"; let b = read_int() in aux r ((name,b)::env)
+      | Print(expr) -> (Printf.printf "%d\n"(eval_expr expr env));aux r env
+      | If(cond,block1,block2) -> if (eval_cond cond env) then let a = aux block1 env in aux r a  else let b = aux block2 env in aux r b
+      | While(cond,block) -> if eval_cond cond env then let a = aux block env in aux ((x,y)::r) a else aux r env
+  in aux prog [] 
+  in ()
